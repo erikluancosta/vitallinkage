7f3168428886258346bd33b4ba77285cd6eb338a
erik <erikluan@alumni.usp.br>
2024-02-26 16:02:32 -0300

    Versão 26/02

--- data-raw/processo_linkage.R
+++ data-raw/processo_linkage.R
@@ -13,12 +13,10 @@ library(writexl)
 library(vitaltable)
 devtools::load_all(".")
 options(scipen = 999)
-devtools::install_github('https://github.com/GitVitalStrategiesBr/VitalBrEpiTools')
-
-vitaltable::tab_1()
 
 load('C:/vitalstrategies/data_sicence/TCC/script_linkage/dados/base_concatenada_rn.RData')
 
+namestand<-vitallinkage::namestand
 
 concat$nu_doc <- gsub("[^0-9]", NA, concat$nu_doc)
 concat$nu_doc[concat$nu_doc == ""] <- NA
@@ -137,37 +135,3 @@ df_r6 <- df |>
   ungroup() |>
   vitallinkage::meio_de_campo()
 
-
-
-
-# Criando dados fictícios
-dados <- data.frame(
-  nome = c(NA, "JOAO PEDRO", "MARIA SANTOS", "MARIA SANTOS","PEDRO", "ANA MARIA", "AMA MARIA", "CARLOS ADAO", "JOAO P", "JAO PEDRO"),
-  data_nascimento = c("1995-01-30","1990-05-15", "1985-10-20",NA, "1988-07-12", "1992-03-08", "1992-08-03", "1995-01-30", "1990-05-15", "1990-05-15"),
-  nome_mae = c("ISABEL SILVA","MARIA CLARA", "SANDRA",'SANDRA', "LUCIA MARIA", "FERNANDA", "FERNANDA", "ISABEL SILVA", NA, "MARIA CLARA"),
-  banco = c('SIM', "SIH", "SINAN", 'SIH', 'SIH','SIH', 'SINAN','SIH', 'SINAN', 'SIM')
-)
-
-dados_2 <- dados |> vitallinkage::soundex_linkage('nome') |>
-  vitallinkage::soundex_linkage('nome_mae') |>
-  select(-nome1, -nome2, -nome3, -nome2_sound, -nome3_sound, -nome_mae1, -nome_mae2, -nome_mae3,-nome_mae1_sound, -nome_mae2_sound, -nome_mae3_sound )
-
-dados_2 <- dados_2 |> vitallinkage::start_linkage(c("data_nascimento"), c('nome_sound'))
-dados_2 <- dados_2 |> vitallinkage::regras_linkage(c("nome_sound"), c('nome_mae_sound'), 2)
-dados_2 <- dados_2 |> vitallinkage::regras_linkage(c("nome_mae_sound"), c('nome_sound'), 3)
-dados_2 <- dados_2 |> vitallinkage::regras_linkage(c("data_nascimento"), c('nome_mae_sound'), 4)
-dados_2 <- dados_2 |> vitallinkage::regras_linkage(c("data_nascimento"), c('nome1_sound'), 5)
-dados_2 <- dados_2 |> select(par_1, everything()) |> arrange(par_1)
-
-
-dados_3 <- dados_2 |>
-  group_by(par_1) |>
-  mutate(
-    data_nascimento_corrigida = ifelse(!is.na(par_1), max(data_nascimento, na.rm=TRUE), data_nascimento),
-    nome_corrigido = ifelse(!is.na(par_1), max(nome, na.rm=TRUE), nome),
-    nome_mae_corrigido = ifelse(!is.na(par_1), max(nome_mae, na.rm = TRUE), nome_mae)
-  )
-
-dados_final <- dados_3 |> select(par_1, nome_corrigido, nome_mae_corrigido, data_nascimento_corrigida, banco)
-
-d <- vitaltable::tab_1(dados_final,nome_corrigido)
--- data-raw/tratamento_SIM.R
+++ data-raw/tratamento_SIM.R
@@ -1,5 +1,3 @@
-## Preparando as bases do SIM
-
 library(readstata13)
 library(tidyverse)
 library(pdftools)
@@ -14,6 +12,28 @@ library(lubridate)
 library(writexl)
 devtools::load_all(".")
 
+
+# Specify the file path
+path <- 'C:/vitalstrategies/data_sicence/TCC/SCRIPTS_LINKAGE/Erik/SIM'
+
+# lista de arquivos com fim .DBF
+dbf_files <- list.files(path, pattern = "\\.DBF$", full.names = TRUE)
+
+# Data Frame para adicionar os dados carregados
+sim_raw <- data.frame()
+
+# Loop para carregar e concatenar os dbf
+for (file in dbf_files) {
+  # Lendo arquivo DBF
+  sim_temp <- read.dbf(file) |>
+    mutate(ANO=substr(DTOBITO,5,8))
+
+  sim_raw <- bind_rows(sim_raw,sim_temp)
+  rm(sim_temp)
+}
+
+sim<-sim_raw
+
 sim_2 <- sim |>
   vitallinkage::limpa_ignorados_sim() |> # Remove textos de ignorado
   vitallinkage::padroniza_variaveis(namestand, "SIM") |> # Padronizando os nomes das variáveis
@@ -21,10 +41,13 @@ sim_2 <- sim |>
   vitallinkage::ano_sim() |> # Adicionando o ano
   vitallinkage::as_char() |> # Transformando todos em character
   vitallinkage::ajuste_txt() |> # Ajusta as variáveis que contem "nome" na composição
-  vitallinkage::soundex_linkage("ds_nome_pac") |> # Criando colunas com SOUNDEX
-  vitallinkage::soundex_linkage("ds_nome_pai") |> # Criando colunas com SOUNDEX
-  vitallinkage::soundex_linkage("ds_nome_mae") |> # Criando colunas com SOUNDEX
+  vitallinkage::soundex_linkage("ds_nome_pac") |>
+  vitallinkage::soundex_linkage("ds_nome_pai") |>
+  vitallinkage::soundex_linkage("ds_nome_mae") |>
   vitallinkage::ajuste_res() |> # Ajusta as variáveis que contem "_res" na composição
+  vitallinkage::soundex_linkage("ds_bairro_res") |>
+  vitallinkage::soundex_linkage("ds_rua_res") |>
+  vitallinkage::soundex_linkage("ds_comple_res") |>
   vitallinkage::drop_duplicados_sim_padronizado() |> # Série de lógicas para remover duplicados
   ## NOVAS VARIÁVEIS
   vitallinkage::ds_raca_sim() |> # Ajustando a raça/cor
@@ -32,4 +55,23 @@ sim_2 <- sim |>
   vitallinkage::nu_idade_anos_sim() |> # Ajustanso a idade em anos
   vitallinkage::as_char()  # Tudo como character
 
-usethis::use_data(sim, overwrite = TRUE)
+# Anonimização
+sim_anon <- sim_2  |>
+  vitallinkage::sim_anon()
+
+
+
+sim_3 <- sim_2 |> vitallinkage::ajuste_res()
+
+sim_a <- sim_2 |> dplyr::select(ds_bairro_res, ds_comple_res, ds_rua_res, cd_cep_res,nu_num_res, cd_mun_res, ds_comple_res)
+
+sim_b <- sim_3 |> dplyr::select(ds_bairro_res, ds_comple_res, ds_rua_res, cd_cep_res,nu_num_res, cd_mun_res, ds_comple_res)
+
+sim_3$ds_comple_res
+a <- sim_a[grepl("[^\\x00-\\x7F] ", sim_a$ds_bairro_res), ]
+b <- sim_b[grepl("[^\\x00-\\x7F]", sim_b$ds_bairro_res), ]+
+
+usethis::use_data_raw("tratamento_SIM")
+
+
+sim <- vitallinkage::padroniza_SIM(sim_raw)
